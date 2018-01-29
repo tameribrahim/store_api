7a01d20a692bbe37bbfd63e132f4eb3c24d208bb
Tamer Ibrahim <tibrahim@tabssoft.com>
2018-01-28 19:00:03 -0500

    Instal jwt

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
 
             new FOS\UserBundle\FOSUserBundle(),
+            new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
